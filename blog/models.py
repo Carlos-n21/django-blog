fec60fde701de7c0fc24d837953a638834180db2
Carlos Neves <c.nsm.714@gmail.com>
2025-01-14 22:14:05 +0000

    update code syntax and structure

--- blog/models.py
+++ blog/models.py
@@ -1,38 +1,29 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Post
 
-STATUS = ((0, "Draft"), (1, "Published"))
+# Create your views here.
 
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        ordering = ["-created_on"]
 
-    def __str__(self):
-        return f"{self.title} | written by {self.author}"
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
 
-class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
-    body = models.TextField()
-    approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ["created_on"]
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
 
-    def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request, "blog/post_detail.html", {"post": post},)
